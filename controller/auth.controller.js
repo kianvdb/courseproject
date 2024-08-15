9e503caa4d69183b9234ee111ec51c25ebd3ea3d
kianvdb <kian.van.den.bussche@student.ehb.be>
2024-08-15 15:18:53 +0200

    trying to fix deployment

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../model/auth.model.js';
 import { generateTokenandSetCookie } from '../middleware/generateTokenandSetCookie.js';
 
